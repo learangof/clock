0f37b59c42fd8d0d8d871556320cd2a20565185e
Luis Arango <learangof@outlook.com>
2021-03-01 17:19:42 +1300

    add logic

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 const DIGITALTIME = document.querySelector("#digital .digital-text");
+const DIGITALDAY = document.querySelector("#date #day");
+const DIGITALMONTH = document.querySelector("#date #month");
+const DIGITALYEAR = document.querySelector("#date #year");
+
 
 function runTheClock() {
     var date =  new Date();
-    console.log(date.toTimeString());
+    //console.log(date.toTimeString());
     let hr = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
@@ -20,6 +24,9 @@ function runTheClock() {
     SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"
 
     DIGITALTIME.innerHTML = checkNumber(hr) + ":" + checkNumber(min) + ":" + checkNumber(sec);
+    DIGITALDAY.innerHTML = checkNumber(date.getDay());
+    DIGITALMONTH.innerHTML = checkNumber(date.getMonth()+1);
+    DIGITALYEAR.innerHTML = date.getFullYear().toString().slice(2);
 }
 
 var interval = setInterval(runTheClock, 1000);
